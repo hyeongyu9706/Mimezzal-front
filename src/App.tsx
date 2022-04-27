1ebcd25b59d3a73b640021a624ab17729984f659
hyeongyu9706 <54524@naver.com>
2022-04-28 01:53:48 +0900

    project-setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-// import { useState } from 'react'
-// import logo from './logo.svg'
 
 import RoutesWrapper from 'Components/Routers/Routes';
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <div className="App">
